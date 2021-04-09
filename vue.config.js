b7925ab9f0e36d9a1972d0dafa7a7c9b915e6566
zky2824193947 <2824193947@qq.com>
2021-04-09 09:24:45 +0800

    项目上线优化3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: '/vue_shop/',
+  publicPath: '/vue_shop/dist/',
   chainWebpack: config => {
     // (发布模式)查找并使用的是cdn资源
     config.when(process.env.NODE_ENV === 'production', config => {
