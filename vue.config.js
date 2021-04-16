4c1ef99dbfa5c9174a360af68dd8d2f40f8de2f7
zky2824193947 <2824193947@qq.com>
2021-04-16 22:14:58 +0800

    r

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: '/',
+  publicPath: '/dist',
   chainWebpack: config => {
     // (发布模式)查找并使用的是cdn资源
     config.when(process.env.NODE_ENV === 'production', config => {
